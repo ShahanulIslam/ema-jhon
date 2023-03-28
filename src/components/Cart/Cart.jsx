7700335c39d9a1826f838302ae1dbe0ddde3fe63
Your Name <shahanricky86@gmail.com>
2023-03-29 00:46:01 +0600

    total

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({cart}) => {
+
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price
+        shipping = shipping +product.shipping
+    }
+    
+    const tex = total/100*7;
+    const grandTotal = total + shipping +tex
+
+    return (
+        <div className='cart'>
+            <h3>Order Summary </h3>
+            <p>Selected item</p>
+            <p>Total Price :${total}</p>
+            <p>Shipping charge : {shipping}</p>
+            <p>Tex: {tex.toFixed(2)}</p>
+            <h5>Grand total: {grandTotal.toFixed(2)} </h5>
+        </div>
+    );
+};
+
+export default Cart;
