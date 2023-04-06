d8f2211e030196749019a80f6a71a593c2b8df91
Your Name <shahanricky86@gmail.com>
2023-04-06 20:19:38 +0600

    add-delete-button

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Cart.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Cart = ({ cart }) => {
+
+const Cart = ({ cart,handleClearCart }) => {
     let total = 0;
     let shipping = 0;
     let quantity = 0;
     for (const product of cart) {
 
-        if(product.quantity ===0){
-            product.quantity =1;
+        if (product.quantity === 0) {
+            product.quantity = 1;
         }
 
         total = total + product.price * product.quantity;
         shipping = shipping + product.shipping * product.quantity;
-        quantity =quantity + product.quantity
+        quantity = quantity + product.quantity
     }
 
     const tex = total / 100 * 7;
@@ -27,6 +30,12 @@ const Cart = ({ cart }) => {
             <p>Shipping charge : {shipping}</p>
             <p>Tex : {tex.toFixed(2)}</p>
             <h5>Grand total: {grandTotal.toFixed(2)} </h5>
+
+                <button onClick={handleClearCart} className='btn-clear-cart'>
+                    <span>Clear cart</span>
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
+            
         </div>
     );
 };
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -3,7 +3,7 @@ import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
 import "./Order.css"
-import { removeFromDb } from '../../utilities/fakedb';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
     const savedCart = useLoaderData()
@@ -14,6 +14,11 @@ const Order = () => {
         const remaining = cart.filter(product=> product.id  !== id);
         setCart(remaining) ;
         removeFromDb(id)
+    }
+
+    const handleClearCart = () =>{
+        setCart([])
+        deleteShoppingCart()    
     }
 
     return (
@@ -29,7 +34,9 @@ const Order = () => {
             </div>
 
             <div className='products-container'>
-                <Cart cart ={cart}></Cart>
+                <Cart cart ={cart}
+                handleClearCart ={handleClearCart}
+                ></Cart>
             </div>
         </div>
     );
